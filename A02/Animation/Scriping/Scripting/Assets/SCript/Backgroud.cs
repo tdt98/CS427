f507075fadfe9ef83d614c1c0b5d9e8e8169c9ca
TU <tdtu@apcs.vn>
2019-06-02 23:39:53 +0700

    A02

---
+++ A02/Animation/Scriping/Scripting/Assets/SCript/Backgroud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroud : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject []candies;
+
+
+    void Start()
+    {
+        int candyposition = Random.Range(0, candies.Length);
+        GameObject candy = Instantiate(candies[candyposition], transform.position, Quaternion.identity);
+        candy.transform.parent = this.transform;
+        candy.name = this.gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ A02/Animation/Scriping/Scripting/Assets/SCript/Node.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private Backgroud[,] allTiles;
+    void Start()
+    {
+        allTiles = new Backgroud[width, height];
+        for(int i=0;i<width;++i)
+            for (int j=0;j<height;++j)
+            {
+                Vector2 temPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, temPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "(" + i + "," + j + ")";
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
